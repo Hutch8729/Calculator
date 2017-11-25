04b2d0933a045137f7ed03fe80c2663908395b4a
Kyle Hutchens <Hutch8729@gmail.com>
2017-11-25 03:55:22 -0500

    skeleton work. need to make changes. get away from the array. do everything live on clicks.

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
 var calcArr = [];
 var answer = 0;
-var lastInput = 0;
+var lastInput = "";
+var a = 0;
+var b = 0;
+var c = "";
+
+$('#btns').on('click', '.btn', this.id, function(){
+	console.log(this.id);
+	if ($(this).hasClass('modsBtn')){
+		calcArr.push(parseInt(lastInput));
+		lastInput = "";
+		$('#array').html(calcArr);
+		calculate();
+		calcArr.push(this.id);
+		$('#array').html(calcArr);
+	}
+
+	if ($(this).hasClass('numBtn')){
+		lastInput += this.id;
+		$('#lastNum').html(lastInput);
+	}
+});
+
+
+function mod(val, type){
+	if (type === "*"){
+		return answer * val;
+	}
+
+	else if (type === "/"){
+		return answer / val;
+	}
+
+	else if (type === "+"){
+		return answer + val;
+	}
+
+	else if (type === "-"){
+		return answer - val;
+	}
+
+	else{
+		return answer;
+	}
+}
+
+function calculate(){
+	for (i = 0; i < calcArr.length; i++){
+		if (i === 0){
+			answer += calcArr[i];
+			$('#answer').html(answer);
+		}
+		if (typeof calcArr[i] === 'number' && i !== 0){
+			answer = mod(calcArr[i], calcArr[i - 1]);
+		}
+	}
+}
