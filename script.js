1d23649070925131ec9cf70d253d9ad33488e81a
Kyle Hutchens <Hutch8729@gmail.com>
2017-12-08 18:01:48 -0500

    starting to not be shit. need to handle numbers being entered. currently 1 + 2 is 0 + 1 for first evaluation

--- script.js
+++ script.js
@@ -1,34 +1,69 @@
 var calcArr = [];
 var answer = 0;
-var lastInput = "";
-var a = 0;
-var b = 0;
-var c = "";
+var lastInputType = "";
+var lastNumInput = 0;
+var lastOperatorInput = "";
+var currInput = "";
 
 $('#btns').on('click', '.btn', this.id, function(){
 	console.log(this.id);
+	console.log("current answer " + answer);
+
+	//check for button is mod
 	if ($(this).hasClass('modsBtn')){
-		calcArr.push(parseInt(lastInput));
-		lastInput = "";
-		$('#array').html(calcArr);
-		calculate();
-		calcArr.push(this.id);
-		$('#array').html(calcArr);
+		//check not . || ce || c
+		if (this.id !== "." && this.id !== "=" && this.id !== "C" && this.id !== "CE"){
+			if (lastInputType === 'number'){
+				lastNumInput = currInput;
+				calcArr.push(lastNumInput);
+				currInput = "";
+				$('#currInput').html(this.id);
+				lastInputType = 'operator';
+				if (lastOperatorInput !== ""){
+					answer = mod(lastNumInput, lastOperatorInput);
+				}
+				lastOperatorInput = this.id;
+			}
+
+			else if (lastInputType === 'operator'){
+				console.log("i'm here");
+				lastOperatorInput = this.id;
+				$('#currInput').html(currInput);
+				lastInputType = 'operator';
+			}
+		}
+
+		else if (this.id === "=" && lastInputType !== ''){
+			answer = mod(lastNumInput, lastOperatorInput);
+			$('#answer').html(answer);
+			lastOperatorInput = "";
+			lastInputType = "operator";
+		}
 	}
 
+	//check for numbtn
 	if ($(this).hasClass('numBtn')){
-		lastInput += this.id;
-		$('#lastNum').html(lastInput);
+		currInput += this.id;
+		$('#currInput').html(currInput);
+		lastInputType = 'number';
 	}
+
+	//update fullEQ on every button press
+	$("#fullEq").html(calcArr.join(" "));
 });
 
 
 function mod(val, type){
+	console.log(answer + " " + type + " " + val);
 	if (type === "*"){
 		return answer * val;
 	}
 
 	else if (type === "/"){
+		if (val === 0){
+			$('#answer').html("undefined");
+			return 0;
+		}
 		return answer / val;
 	}
 
@@ -39,13 +74,10 @@ function mod(val, type){
 	else if (type === "-"){
 		return answer - val;
 	}
-
-	else{
-		return answer;
-	}
 }
 
 function calculate(){
+	if (typeof lastInput)
 	for (i = 0; i < calcArr.length; i++){
 		if (i === 0){
 			answer += calcArr[i];
@@ -55,4 +87,8 @@ function calculate(){
 			answer = mod(calcArr[i], calcArr[i - 1]);
 		}
 	}
+}
+
+function checkDecimal(){
+	//check for multiple decimals && first character decimal add leading 0
 }
